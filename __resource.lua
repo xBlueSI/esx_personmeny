5dece8bba45d97064e54d21ac78dc798198956ca
FiskN <36334995+xBlueSI@users.noreply.github.com>
2018-08-08 00:12:50 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,18 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+description 'En person meny gjord av xBlueSI'
+
 client_scripts {
-	'client.lua'
+	'client.lua',
+	'@es_extended/locale.lua',
+	'locales/sv.lua',
+	'locales/en.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/sv.lua',
+	'locales/en.lua',
+	'config.lua'
 }
