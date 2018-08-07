3ca306eaa9a1c19553739081dc39cb3a49c1df51
FiskN <36334995+xBlueSI@users.noreply.github.com>
2018-08-08 00:13:36 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,47 @@
+Locales['en'] = {
+	-- Citizen Interactions
+	["citizen_interactions"] 	= "Citizen Interactions",
+	["vehicle_menu"]			= "Vehicle Menu",
+	["id_card"]					= "ID-Card",
+	["pee"]						= "Pee",
+	["poop"]					= "Poop",
+	["blindfold"]				= "On/Off blindfold",
+	["nobody_near"]				= "No players nearby",
+	-- ID Meny
+	["check_id"]				= "Check your ID",
+	["show_id"]					= "Show your ID",
+	["id_menu"]					= "ID menu",
+	-- Fordonsmeny
+	["engine"]					= "Engine",
+	["doors"]					= "Doors",
+	["window"]					= "Window",
+	["cruise_control"]			= "Cruise Control",
+	["shuff"]					= "Change Seat",
+	["turned_off_engine"]		= "You turned off the engine",
+	["turned_on_engine"]		= "You turned on the engine",
+	-- Dörrar
+	["hood"]					= "Hood",
+	["trunk"]					= "Trunk",
+	["front_left_door"]			= "Left front door",
+	["front_right_door"]		= "Right front door",
+	["back_left_door"]			= "Left back door",
+	["back_right_door"]			= "Right back doot",
+	["closed_hood"]				= "You closed the hood!",
+	["opened_hood"]				= "You opened the hood!",
+	["closed_door"]				= "You opened the door",
+	["opened_door"]				= "You closed the door!",
+	-- Fönster meny
+	["window_menu"]				= "Window Menu",
+	["front_left_window"]		= "Left right window",
+	["front_right_window"]		= "Right front window",
+	["back_left_window"]		= "Left back window",
+	["back_right_window"]		= "Right back door",
+	["opened_window"]			= "You closed the window",
+	["closed_wondow"]			= "You opened the window",
+	-- Fartbegränsare
+	["disabled"]				= "Disabled",
+	["disabled_cruise_control"]	= "You turned off the cruise control",
+	-- Seatbelt
+	["seatbelt_on"]				= "Fasten seatbelt!"
+	["seatbelt_off"]			= "Unbuckled seatbelt!"
+}
